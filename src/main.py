3c5a82cd57a9bc5d3be7e7873bb86aa8f6c88bd8
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2023-05-04 15:32:19 +0200

    Add training code from Restaurant Sentiment Analysis Notebook

---
+++ src/main.py
@@ -0,0 +1,73 @@
+# Importing libraries
+import numpy as np
+import pandas as pd
+
+import re                           # re = Secret Labs' Regular Expression Engine
+import nltk                         # NLTK = Natural Language Toolkit
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+import pickle
+import joblib
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Variables
+path_to_input_data = '../data/input/'
+path_to_output = '../data/output/'
+dataset_name = 'a1_RestaurantReviews_HistoricDump.tsv'
+path_to_dataset = path_to_input_data + dataset_name
+
+# Importing dataset
+dataset = pd.read_csv(path_to_dataset, delimiter = '\t', quoting = 3)
+print(dataset.shape)
+
+if __name__ == '__main__':
+    print("hello")
+
+    # Data Preprocessing
+    # nltk.download('stopwords')
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    all_stopwords.remove('not')
+
+    corpus = []
+
+    for i in range(0, 900):
+        review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+        review = review.lower()
+        review = review.split()
+        review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+        review = ' '.join(review)
+        corpus.append(review)
+
+    # Data transformation
+    cv = CountVectorizer(max_features=1420)
+    X = cv.fit_transform(corpus).toarray()
+    y = dataset.iloc[:, -1].values
+
+    # Saving BoW dictionary to later use in prediction
+    bow_path = path_to_output + 'c1_BoW_Sentiment_Model.pkl'
+    pickle.dump(cv, open(bow_path, "wb"))
+
+    # Dividing dataset into training and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+    # Model fitting (Naive Bayes)
+    classifier = GaussianNB()
+    classifier.fit(X_train, y_train)
+
+    # Exporting NB Classifier to later use in prediction
+    joblib.dump(classifier, path_to_output + 'c2_Classifier_Sentiment_Model')
+
+    y_pred = classifier.predict(X_test)
+
+    # Model performance
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    print(accuracy_score(y_test, y_pred))
+
